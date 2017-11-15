42942bd20e6fcc0f1fd369db554d0c7f6ee930e5
JoaquimAlfredoRibeiro <joaquimalfredoribeiro@gmail.com>
2017-11-15 20:04:48 +0000

    added Pet domain comment

--- src/main/java/domain/Pet.java
+++ src/main/java/domain/Pet.java
@@ -1,16 +1,13 @@
 package domain;
 
-import lombok.*;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-@ToString
 public class Pet {
 
-    private Integer id;
+
     private String name;
+
     private int gender;
-    private int owner_id;
+
+    //teste branch f
+
 }
